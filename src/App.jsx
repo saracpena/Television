1967ad368c781339dea9fac31942a4ae427881e3
Sara <s.cristal.p@gmail.com>
2026-06-30 19:56:13 +0200

    added imports and debugged an HMR issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 /**
- * React TV is an web streaming platform that allows users to browse
+ * React TV is a web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { tvShows } from "./shows/data";
+import EpisodeDetails from "./episodes/EpisodeDetails";
+import EpisodeList from "./episodes/EpisodeList";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelections from "./shows/ShowSelection";
+
 export default function App() {
   return (
     <>
       <header>
         <p>React TV</p>
+        <EpisodeDetails />
       </header>
       <main></main>
     </>
