4559f1198d80f1c62fafa6a5fc634cea59855217
Sara <s.cristal.p@gmail.com>
2026-07-01 20:33:09 +0200

    imports and state setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
  * React TV is a web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
 import { tvShows } from "./shows/data";
 import EpisodeDetails from "./episodes/EpisodeDetails";
 import EpisodeList from "./episodes/EpisodeList";
@@ -9,13 +10,17 @@ import ShowDetails from "./shows/ShowDetails";
 import ShowSelections from "./shows/ShowSelection";
 
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null); 
   return (
     <>
       <header>
         <p>React TV</p>
         <EpisodeDetails />
       </header>
-      <main></main>
+      <main>
+        <ShowSelections onShowSelect={setSelectedShow} />
+        {selectedShow && <ShowDetails show={selectedShow} />}
+      </main>
     </>
   );
 }
